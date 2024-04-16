c978c305ae0f121fa240c7b2f22ebd27333ee8a5
kimgusxo <81300338+kimgusxo@users.noreply.github.com>
2024-04-16 17:51:44 +0900

    StatLottoRepositoryUnitTest green 작성

--- src/main/java/com/example/lotto/repository/StatLottoRepository.java
+++ src/main/java/com/example/lotto/repository/StatLottoRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StatLottoRepository extends MongoRepository<String, StatLotto> {
+public interface StatLottoRepository extends MongoRepository<StatLotto, String> {
 
 }
--- src/test/java/com/example/lotto/unit/repository/StatLottoRepositoryUnitTest.java
+++ src/test/java/com/example/lotto/unit/repository/StatLottoRepositoryUnitTest.java
@@ -1,4 +1,158 @@
 package com.example.lotto.unit.repository;
 
+import com.example.lotto.domain.Result;
+import com.example.lotto.domain.StatLotto;
+import com.example.lotto.repository.StatLottoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DataMongoTest
 public class StatLottoRepositoryUnitTest {
+
+    @Autowired
+    private StatLottoRepository statLottoRepository;
+
+    @Nested
+    @DisplayName("find 테스트")
+    class Test_Find {
+
+        @BeforeEach
+        @DisplayName("데이터 설정")
+        void setUp() {
+            Integer number = 1;
+            Integer count = 1;
+            Double probability = 1.1111;
+            Integer bonusCount = 1;
+            Double bonusProbability = 1.1111;
+
+            Optional<Result> existingStatLotto = Optional.ofNullable(statLottoRepository.findByNumber(number));
+
+            if(existingStatLotto.isEmpty()) {
+                StatLotto statLotto = StatLotto.builder()
+                        .number(number)
+                        .count(count)
+                        .probability(probability)
+                        .bonusCount(bonusCount)
+                        .bonusProbability(bonusProbability)
+                        .build();
+
+                statLottoRepository.save(statLotto);
+            }
+        }
+
+        @Nested
+        @DisplayName("findByNumber 테스트")
+        class Test_FindByNumber {
+            @Test
+            @DisplayName("성공")
+            void success() {
+                // given
+                Integer number = 1;
+
+                // when
+                StatLotto statLotto = statLottoRepository.findByNumber(number);
+
+                // then
+                assertThat(statLotto.getNumber())
+                        .isEqualTo(number);
+
+            }
+
+            @Test
+            @DisplayName("실패")
+            void fail() {
+                // given
+                Integer number = 0;
+
+                // when
+                StatLotto statLotto = statLottoRepository.findByNumber(number);
+
+                // then
+                assertThat(statLotto)
+                        .isNull();
+            }
+        }
+
+        @Nested
+        @DisplayName("findByNumberList 테스트")
+        class Test_FindByNumberList {
+
+            @Test
+            @DisplayName("성공")
+            void success() {
+                // given
+                Integer number = 1;
+                List<Integer> numberList = new ArrayList<>(Arrays.asList(number));
+
+                // when
+                List<StatLotto> statLottoList = statLottoRepository.findByNumberList(numberList);
+
+                // then
+                assertThat(statLottoList)
+                        .isNotEmpty();
+            }
+
+            @Test
+            @DisplayName("실패")
+            void fail() {
+                // given
+                Integer number = 0;
+                List<Integer> numberList = new ArrayList<>(Arrays.asList(number));
+
+                // when
+                List<StatLotto> statLottoList = statLottoRepository.findByNumberList(numberList);
+
+                // then
+                assertThat(statLottoList)
+                        .isEmpty();
+
+            }
+
+        }
+
+        @Nested
+        @DisplayName("findAll 테스트")
+        class Test_FindAll {
+            @Test
+            @DisplayName("성공")
+            void success() {
+                // given
+
+                // when
+                List<StatLotto> statLottoList = statLottoRepository.findAll();
+
+                // then
+                assertThat(statLottoList)
+                        .isNotEmpty();
+            }
+
+            @Test
+            @DisplayName("실패")
+            void fail() {
+                // given
+
+                // when
+                List<StatLotto> statLottoList = statLottoRepository.findAll();
+
+                // then
+                assertThat(statLottoList)
+                        .isEmpty();
+            }
+        }
+
+    }
+
+
 }
